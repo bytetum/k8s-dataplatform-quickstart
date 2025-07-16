084464bc4ed610d7526d30b6fb091f04c3e3fb49
thanh <thanh.hoangxuan@alfalaval.com>
2025-07-16 23:56:14 +0700

    21

--- gitops/Applications/ExternalSecrets.cs
+++ gitops/Applications/ExternalSecrets.cs
@@ -7,7 +7,6 @@ internal class ExternalSecrets
         new ArgoApplicationBuilder("external-secrets", provider)
             .Type(ApplicationType.Chart)
             .SyncWave(0)
-            .Branch("0.18.2")
             .HelmValues("https://charts.external-secrets.io")
             .Build();
     }
