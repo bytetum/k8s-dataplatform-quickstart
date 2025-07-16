e57ee88d404975d4206f0765e714e8a1a3680bf6
thanh <thanh.hoangxuan@alfalaval.com>
2025-07-16 23:19:00 +0700

    15

--- gitops/Applications/ArgoApplication.cs
+++ gitops/Applications/ArgoApplication.cs
@@ -1,28 +1,41 @@
-﻿// File: ArgoApplicationBuilder.cs
-using Pulumi;
+﻿using Pulumi;
 using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
-using System.Text;
 
 namespace argocd.applications;
 
-// The builder class, now with Helm-specific capabilities.
 internal class ArgoApplicationBuilder(string name, Pulumi.Kubernetes.Provider provider)
 {
     private string project = "default";
     private string? destinationNamespace;
     private ApplicationType applicationType = ApplicationType.Yaml;
-    private string targetRevision = "HEAD"; // Default for Git, should be overridden for Helm
+    private string branch = "HEAD";
     private string path = $"gitops/manifests/{name}";
     private string repoURL = "git@github.com:bytetum/k8s-dataplatform-quickstart.git";
     private int syncWave = 0;
-    private string? helmValues;
-    private bool addFinalizer = true;
-
+    private string helmValues;
+    
     public ArgoApplicationBuilder SyncWave(int syncWave)
     {
         this.syncWave = syncWave;
         return this;
     }
+    
+    public ArgoApplicationBuilder HelmValues(string values)
+    {
+        this.helmValues = values;
+        return this;
+    }
+    
+    private InputMap<string> Annotation => new()
+    {
+        { "argocd.argoproj.io/sync-wave", syncWave.ToString() },
+    };
+
+    private InputMap<string> Destination => new()
+    {
+        { "server", "https://kubernetes.default.svc" },
+        { "namespace", destinationNamespace ?? name }
+    };
 
     public ArgoApplicationBuilder Project(string project)
     {
@@ -42,100 +55,86 @@ internal class ArgoApplicationBuilder(string name, Pulumi.Kubernetes.Provider pr
         return this;
     }
 
-    // Use this for Git branches or Helm chart versions.
-    public ArgoApplicationBuilder TargetRevision(string targetRevision)
+    public ArgoApplicationBuilder Branch(string branch)
     {
-        this.targetRevision = targetRevision;
+        this.branch = branch;
         return this;
     }
-    
-    // Use this to specify the Helm repository URL.
+
     public ArgoApplicationBuilder RepoUrl(string repoURL)
     {
         this.repoURL = repoURL;
         return this;
     }
 
-    // New method to provide Helm values as a YAML string.
-    public ArgoApplicationBuilder HelmValues(string values)
-    {
-        this.helmValues = values;
-        return this;
-    }
-
-    public ArgoApplicationBuilder WithFinalizer(bool enable)
-    {
-        this.addFinalizer = enable;
-        return this;
-    }
-
     public void Build()
     {
-        var annotations = new InputMap<string>
+        var source = applicationType switch
         {
-            { "argocd.argoproj.io/sync-wave", syncWave.ToString() },
-        };
-
-        var finalizers = new InputList<string>();
-        if (addFinalizer)
-        {
-            finalizers.Add("resources-finalizer.argoproj.io");
-        }
-
-        // --- SOURCE LOGIC ---
-        // We build a single source object and populate it based on the type.
-        var sourceArgs = new ArgoApplicationSourceArgs
-        {
-            RepoUrl = repoURL,
-            TargetRevision = targetRevision,
-        };
-
-        if (applicationType == ApplicationType.Helm)
-        {
-            sourceArgs.Chart = name; // Assumes chart name is the same as the app name
-            if (!string.IsNullOrEmpty(helmValues))
+            ApplicationType.Yaml => new ArgoApplicationSourceArgs
             {
-                sourceArgs.Helm = new ArgoHelmArgs { Values = helmValues };
-            }
-        }
-        else // Yaml type
-        {
-            sourceArgs.Path = path;
-            sourceArgs.Directory = new InputMap<bool> { { "recurse", true } };
-        }
-
-        // --- SYNC POLICY LOGIC ---
-        var syncPolicy = new ArgoApplicationSyncPolicyArgs
-        {
-            Automated = new InputMap<bool>
-            {
-                { "prune", true },
-                { "selfHeal", true },
+                Path = path,
+                RepoUrl = repoURL,
+                Branch = branch,
+                Directory =
+                {
+                    { "recurse", true }
+                }
             },
-            // Add CreateNamespace=true only for Helm apps, a common pattern.
-            SyncOptions = applicationType == ApplicationType.Helm
-                ? new InputList<string> { "CreateNamespace=true" }
-                : new InputList<string>()
+            ApplicationType.Helm => new ArgoHelmApplicationSourceArgs
+            {
+                Chart = name,
+                RepoUrl = repoURL,
+                Branch = branch,
+                SkipCrds = false,
+            },
+            ApplicationType.Chart => new ArgoHelmApplicationSourceArgs
+            {
+                Chart = name,
+                RepoUrl = "",
+                Helm = helmValues,
+                Branch = branch,
+                SkipCrds = false,
+            }
         };
 
-        _ = new Pulumi.Kubernetes.ApiExtensions.CustomResource(name, new ArgoApplicationArgs
+        var syncPolicy = applicationType switch
+        {
+            ApplicationType.Yaml => new ArgoApplicationSyncPolicyArgs
+            {
+                Automated = new InputMap<bool>
+                {
+                    { "prune", true },
+                    { "selfHeal", true },
+                },
+            },
+            ApplicationType.Helm => new ArgoApplicationSyncPolicyArgs
+            {
+                Automated = new InputMap<bool>
+                {
+                    { "prune", true },
+                    { "selfHeal", true },
+                },
+                SyncOptions =
+                [
+                    "CreateNamespace=true",
+                ],
+            },
+        };
+
+        var application = new Pulumi.Kubernetes.ApiExtensions.CustomResource(name, new ArgoApplicationArgs
         {
             Metadata = new ObjectMetaArgs
             {
+                Annotations = Annotation,
                 Name = name,
-                Namespace = "argocd", // Applications are always in the argocd namespace
-                Annotations = annotations,
-                Finalizers = finalizers,
+                Namespace = "argocd",
             },
             Spec = new ArgoApplicationSpecArgs
             {
                 Project = project,
-                Source = sourceArgs,
-                Destination = new InputMap<string>
-                {
-                    { "server", "https://kubernetes.default.svc" },
-                    { "namespace", destinationNamespace ?? name }
-                },
+                Source = source,
+                Destination = Destination,
                 SyncPolicy = syncPolicy,
             }
         }, new()
@@ -148,14 +147,12 @@ internal class ArgoApplicationBuilder(string name, Pulumi.Kubernetes.Provider pr
 enum ApplicationType
 {
     Yaml,
-    Helm
+    Helm,
+    Chart,
 }
 
-// --- ARGUMENT CLASSES ---
-// These classes model the structure of the Argo CD Application CRD.
-
 internal class ArgoApplicationArgs : Pulumi.Kubernetes.ApiExtensions.CustomResourceArgs
-{
+{ 
     [Input("spec")] public required Input<ArgoApplicationSpecArgs> Spec { get; set; }
 
     public ArgoApplicationArgs()
@@ -167,35 +164,37 @@ internal class ArgoApplicationArgs : Pulumi.Kubernetes.ApiExtensions.CustomResou
 internal class ArgoApplicationSpecArgs : ResourceArgs
 {
     [Input("project")] public required Input<string> Project { get; set; }
+
     [Input("source")] public required Input<ArgoApplicationSourceArgs> Source { get; set; }
+
     [Input("destination")] public InputMap<string> Destination { get; set; } = [];
+
     [Input("syncPolicy")] public required Input<ArgoApplicationSyncPolicyArgs> SyncPolicy { get; set; }
-}
-
-// A single class for the 'source' block, containing optional fields for Git and Helm.
-internal class ArgoApplicationSourceArgs : ResourceArgs
-{
-    [Input("repoURL")] public required Input<string> RepoUrl { get; set; }
-    [Input("targetRevision")] public required Input<string> TargetRevision { get; set; }
-
-    // Helm-specific fields
-    [Input("chart")] public Input<string>? Chart { get; set; }
-    [Input("helm")] public Input<ArgoHelmArgs>? Helm { get; set; }
-
-    // Git-specific fields
-    [Input("path")] public Input<string>? Path { get; set; }
-    [Input("directory")] public InputMap<bool>? Directory { get; set; }
-}
-
-// New class to represent the 'helm' object within the source.
-internal class ArgoHelmArgs : ResourceArgs
-{
-    [Input("values")]
-    public Input<string>? Values { get; set; }
 }
 
 internal class ArgoApplicationSyncPolicyArgs : ResourceArgs
 {
     [Input("automated")] public required InputMap<bool> Automated { get; set; }
+
     [Input("syncOptions")] public InputList<string> SyncOptions { get; set; } = [];
+}
+
+internal class ArgoApplicationSourceArgs : ResourceArgs
+{
+    [Input("path")] public Input<string> Path { get; set; } = "";
+
+    [Input("repoURL")] public required Input<string> RepoUrl { get; set; }
+
+    [Input("targetRevision")] public Input<string> Branch = "HEAD";
+
+    [Input("directory")] public InputMap<bool> Directory { get; set; } = [];
+}
+
+internal class ArgoHelmApplicationSourceArgs : ArgoApplicationSourceArgs
+{
+    [Input("helm")] public Input<string>? Helm { get; set; }
+    
+    [Input("chart")] public required Input<string> Chart { get; set; }
+
+    [Input("skipCrds")] public required Input<bool> SkipCrds { get; set; }
 }
--- gitops/Applications/ExternalSecrets.cs
+++ gitops/Applications/ExternalSecrets.cs
@@ -7,7 +7,7 @@ internal class ExternalSecrets
         new ArgoApplicationBuilder("external-secrets", provider)
             .Type(ApplicationType.Helm)
             .SyncWave(0)
-            .TargetRevision("0.18.2")
+            .Branch("0.18.2")
             .HelmValues("https://charts.external-secrets.io")
             .Build();
     }
