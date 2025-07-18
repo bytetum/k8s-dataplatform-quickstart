06f4b4e724fd4c517ac99fc5e8572922c3ec8300
thanh <thanh.hoangxuan@alfalaval.com>
2025-07-18 11:27:16 +0700

    38

---
+++ gitops/Applications/WarpStream.cs
@@ -0,0 +1,14 @@
+namespace argocd.applications;
+
+internal class WarpStream
+{
+    public WarpStream(Pulumi.Kubernetes.Provider provider)
+    {
+        new ArgoApplicationBuilder("warpstream-agent", provider)
+            .Type(ApplicationType.Helm)
+            .RepoUrl("https://warpstreamlabs.github.io/charts")
+            .Branch("0.15.28")
+            .InNamespace("warpstream-poc")
+            .Build();
+    }
+}
