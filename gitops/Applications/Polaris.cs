24e7c88230110e3a23e66827b666f8a81ae945d7
thanh <thanh.hoangxuan@alfalaval.com>
2025-07-21 14:33:08 +0700

    45

--- gitops/Applications/Polaris.cs
+++ gitops/Applications/Polaris.cs
@@ -7,6 +7,7 @@ public class Polaris
         new ArgoApplicationBuilder("polaris", provider)
             .Type(ApplicationType.Helm)
             .SyncWave(2)
+            .Branch("1.0.0-incubating")
             .RepoUrl("https://downloads.apache.org/incubator/polaris/helm-chart/1.0.0-incubating/")
             .Build();
     }
