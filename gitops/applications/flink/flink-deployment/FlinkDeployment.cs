35c34da1dea7d5c8301092c28cde0d4e360e84ed
thanh <thanh.hoangxuan@alfalaval.com>
2025-09-08 11:15:48 +0700

    test

--- gitops/applications/flink/flink-deployment/FlinkDeployment.cs
+++ gitops/applications/flink/flink-deployment/FlinkDeployment.cs
@@ -270,7 +270,7 @@ internal class FlinkDeployment : ComponentResource
                     ["job"] = new Dictionary<string, object>
                     {
                         ["jarURI"] =
-                            "local:///opt/flink/lib/*:local:///opt/flink//opt/flink-sql-client-1.20.2.jar:local:///opt/flink/opt/*",
+                            "https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-client/1.20.0/flink-sql-client-1.20.0.jar",
                         ["entryClass"] = "org.apache.flink.table.client.SqlClient",
                         ["args"] = new[]
                         {
