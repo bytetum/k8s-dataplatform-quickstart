5e288c7b0a8b354309078a6b5d9e6dc7da2eab47
thanh <thanh.hoangxuan@alfalaval.com>
2025-09-16 11:33:17 +0700

    hive catalog config file

--- gitops/applications/flink/flink-deployment/FlinkDeployment.cs
+++ gitops/applications/flink/flink-deployment/FlinkDeployment.cs
@@ -328,36 +328,6 @@ namespace applications.flink.flink_deployment
                 Provider = provider,
                 Parent = this
             });
-            var flinkGatewayService = new Service("flink-sql-gateway-service", new ServiceArgs
-            {
-                Metadata = new ObjectMetaArgs
-                {
-                    Name = "flink-sql-gateway-service",
-                    Namespace = Constants.Namespace
-                },
-                Spec = new Pulumi.Kubernetes.Types.Inputs.Core.V1.ServiceSpecArgs
-                {
-                    Type = "ClusterIP",
-                    Ports = new List<Pulumi.Kubernetes.Types.Inputs.Core.V1.ServicePortArgs>
-                    {
-                        new Pulumi.Kubernetes.Types.Inputs.Core.V1.ServicePortArgs
-                        {
-                            Name = "hiveserver2",
-                            Port = 10000,
-                            TargetPort = 10000
-                        }
-                    },
-                    Selector = new Dictionary<string, string>
-                    {
-                        { "app", "basic-checkpoint-ha-sql-example" },
-                        { "component", "jobmanager" }
-                    }
-                }
-            }, new CustomResourceOptions
-            {
-                Provider = provider,
-                Parent = this
-            });
         }
     }
 }
