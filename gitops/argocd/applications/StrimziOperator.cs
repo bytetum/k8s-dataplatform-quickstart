dfeb6d00728e430699618b6b2931ccb3890194cc
thanh <thanh.hoangxuan@alfalaval.com>
2025-09-23 13:50:02 +0700

    Add ArgoCD application for Strimzi Kafka Operator

--- gitops/argocd/applications/StrimziOperator.cs
+++ gitops/argocd/applications/StrimziOperator.cs
@@ -5,9 +5,12 @@ internal class StrimziOperator
     public StrimziOperator(Kubernetes.Provider provider)
     {
         new ArgoApplicationBuilder("strimzi-kafka-operator", provider)
-            .AddSource(ApplicationType.Helm)
-            .RepoUrl("quay.io/strimzi-helm")
+            .AddSource(ApplicationType.Helm
             .Branch("0.47.0")
+            .RepoUrl("quay.io/strimzi-helm")
+            .AddValueFile("$values/gitops/manifests/polaris/values.yaml")
+            .AddSource(ApplicationType.Yaml)
+            .AsValueSource("values")
             .SyncWave(1)
             .Build();
     }
