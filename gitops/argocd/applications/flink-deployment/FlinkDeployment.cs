b285930e84ef8e9a8a80274741ce50beac677f9e
thanh <thanh.hoangxuan@alfalaval.com>
2026-01-22 10:58:44 +0700

    Rename SQL Runner example manifest to `sql-runner-example-script` and update references in Flink deployment.

--- gitops/argocd/applications/flink-deployment/FlinkDeployment.cs
+++ gitops/argocd/applications/flink-deployment/FlinkDeployment.cs
@@ -4,7 +4,7 @@ internal class FlinkDeployment
 {
     public FlinkDeployment(Kubernetes.Provider provider)
     {
-        new ArgoApplicationBuilder("sql-runner-example", provider)
+        new ArgoApplicationBuilder("sql-runner-example-script", provider)
             .SyncWave(2)  // Flink must deploy before Kafka Connect to produce schemas
             .Build();
     }
